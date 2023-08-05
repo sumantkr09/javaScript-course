62896348ed63204a1d3cfcd3dc215d9839ad33ee
sumantkr09 <124792962+sumantkr09@users.noreply.github.com>
2023-08-05 11:29:48 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat all js code as newer version
+
+// alert("Hello")    Here,error.Because we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Sumant")
+
+let name = "Sumant"
+let age = 19
+let isLoggedIn =  false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// Object
+
+console.log(typeof "Sumant");
+console.log(typeof age);
+console.log(typeof null);  //Output of this will be "object"
+console.log(typeof undefined); //output of this will be undefined
+
