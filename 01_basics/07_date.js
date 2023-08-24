6941030635c98958acb9443305b8aa7dadf482fd
sumantkr09 <124792962+sumantkr09@users.noreply.github.com>
2023-08-24 19:42:09 +0000

    date in js

---
+++ 01_basics/07_date.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(typeof(myDate));    //Object
+
+// let myCreatedDate = new Date(2023,0,9)
+// let myCreatedDate = new Date("2023-01-09")
+let myCreatedDate = new Date("09-12-2004")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
+console.log(Math.floor(Date.now()/1000));
+
