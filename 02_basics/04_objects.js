c8803db9c1cbb5c366a6de590a3fc6dc47d87846
sumantkr09 <124792962+sumantkr09@users.noreply.github.com>
2023-08-31 06:30:00 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,69 @@
+// Singleton object
+
+const user = new Object()
+console.log(user);            //{}
+
+
+user.id = "123abc";
+user.name = "abc";
+user.isLoggedIn = false;
+
+console.log(user);
+
+const regularUser = {
+    emal: "sumant@google",
+    name: {
+        fullName:{
+            firstName: "Sumant",
+            lastName: "kumar"
+        }
+    }
+}
+// console.log(regularUser);
+console.log(regularUser.name);
+console.log(regularUser.name.fullName.firstName);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2);      //To concat two object
+const obj3 = {...obj1, ...obj2};                    //Better way to concat
+console.log(obj3);
+
+const user1 = [
+    {
+        id: "1",
+        email: "some...@gmail"
+    },
+    {
+        id: "1",
+        email: "some1@gmail"
+    },
+    {
+        id: "1",
+        email: "some@gmail"
+    },
+    {
+        id: "1",
+        email: "some@gmail"
+    }
+]
+console.log(user1[1].email);
+console.log(user1[2].email);
+console.log(user1[0].email);
+
+console.log(Object.keys(user));      //It will print the key of user object in array form
+console.log(Object.values(user));      //It will print the vaue of user object in array form
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty('isLoggedIn'))             //true
+console.log(user.hasOwnProperty('isLogged'))               //false
+
+
+
