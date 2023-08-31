206508e2f0e8b43c2e7064926a0fc5f1886718c0
sumantkr09 <124792962+sumantkr09@users.noreply.github.com>
2023-08-31 07:46:44 +0000

    funtion

---
+++ 03_basics/01_function.js
@@ -0,0 +1,16 @@
+function name(){
+     console.log("S");
+     console.log("U");
+     console.log("M");
+     console.log("A");
+     console.log("N");
+     console.log("T");
+}
+name()
+
+
+function addTwoNumber(number1, number2){
+    console.log(number1+number2);
+}
+addTwoNumber(3, 8)
+ 
