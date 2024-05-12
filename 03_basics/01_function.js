940c875f232e403540e7e2cd346a57c3fdfb162a
sumantkr09 <124792962+sumantkr09@users.noreply.github.com>
2024-05-12 06:02:22 +0000

    function_basics

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -13,4 +13,33 @@ function addTwoNumber(number1, number2){
     console.log(number1+number2);
 }
 addTwoNumber(3, 8)
+
+// Handling Object in function
+
+const myObj ={
+    name:"Sumant kumar",
+    email:"sumant@gmail.com"
+}
+
+function handlingObj(anyObj){
+    console.log(anyObj.email);
+    console.log(anyObj.age);
+
+}
+// handlingObj(myObj)
+handlingObj({
+    age: 21,
+    isLoggedIn: false
+})
+
+
+// Handling array in function
+
+let myArray = [100,200,300]
+function print(arrayPrinting){
+    console.log(arrayPrinting[1]);
+}
+print(myArray)
+
+
  
